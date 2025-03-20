4e03ee8551ecdd83c7b963d7580e5dfa1d7765e2
ntq1311 <151548272+ntq1311@users.noreply.github.com>
2025-03-21 03:04:16 +0700

    code phần phịm chiếu

--- duAnTotNghiep/src/app/movies/page.tsx
+++ duAnTotNghiep/src/app/movies/page.tsx
@@ -3,12 +3,7 @@ import styles from "./order.module.css"
 import Section from "../Components/Section/Section";
 export default function Order() {
   return (
-    <div className={`container ${styles['container']}`}>
-      
-      <Section title="Phim đang chiếu" filter={(movies)=>movies.status === 1}/> 
-      <Section title="Phim sắp chiếu" filter={(movies)=>movies.status === 2}/> 
 
-       
     </div>
   )
 }
