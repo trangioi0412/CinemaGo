fe25d8b0351ed55db5963a0bf37669d39d86d1f5
lam-2005 <lam2k5phan@gmail.com>
2025-04-17 20:26:43 +0700

    delete-folder

--- duAnTotNghiep/src/app/adminMini/film/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Film = () => {
-    return <div>
-        Đây là trang Film
-    </div>
-}
-
-export default Film;
--- duAnTotNghiep/src/app/adminMini/room/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Room = () => {
-    return <div>
-        Đây là trang phòng chiếu
-    </div>
-}
-
-export default Room;
